df713944edd4e2a767cddf21236455d7cd756434
yangkeloff <yangkeloff@foxmail.com>
2022-01-25 15:03:53 +0800

    vue input两位小数

---
+++ src/pages/index/components/index-view.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <a href="./loadmore.html">loadmore</a>
+    <a href="./dialogue.html">dialogue</a>
+    <a href="./listview.html">listview</a>
+    <section class="t1 center">
+      test
+    </section>
+    <input 
+      class="ipt_1"
+      placeholder="限制小数点后两位" 
+      type="text" 
+      v-model="count"
+      @keydown="checkKeydown($event, count)"
+      @input="count=/^\d+\.?\d{0,2}$/.test(count)||count == '' ? count : count=checkValue">
+  </div>
+</template>
+
+<style lang="less" rel="stylesheet/less" >
+  @import '~common/css/base.less';
+
+  #app {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .t1 {
+    background-color: aqua;
+    width: 500 * @rpx;
+    height: 500 * @rpx;
+  }
+
+  .ipt_1 {
+    border-radius: 10 * @rpx;
+    padding: 10 * @rpx;
+    margin-top: 20 * @rpx;
+  }
+</style>
+
+<dependency component="loadmore" src="common/components/loadmore.vue" lazy />
+
+<script type="text/javascript">
+  import { Component } from '@ali/kylin-framework';
+
+  @Component
+  export default class LoadmoreView {
+    data = {
+      count: '',
+      checkValue: ''
+    }
+    methods = {
+      checkKeydown(e, value) {
+        this.checkValue = value;
+      }
+    }
+  }
+
+</script>
